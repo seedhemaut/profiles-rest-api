41002543da49722bf9f608d2187ce82488781b0f
someone <someone@someplace.com>
2018-06-25 15:20:02 +0530

    Added permissions and search filter

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -5,8 +5,12 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+
 from . import serializers
 from . import models
+from . import permissions
 # Create your views here.
 
 class HelloAPIView(APIView):
@@ -109,3 +113,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     serializer_class=serializers.UserProfileSerilizer
     queryset = models.UserProfile.objects.all()
+    authentication_classes=(TokenAuthentication,)
+    permission_classes=(permissions.UpdateOwnProfile,)
+    filter_backends=(filters.SearchFilter,)
+    search_fields=('name','email',)
